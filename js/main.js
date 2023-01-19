9cfa7d29d1e8ad40ee9d609a05d252fb658bdc48
Raccoon254 <tomsteve187@gmail.com>
2023-01-19 15:34:32 +0300

    Created The Homepage

--- js/main.js
+++ js/main.js
@@ -1,40 +1,68 @@
-
 (function ($) {
     "use strict";
 
-    
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
+    var terms = $('#terms');
+    var referralCode = $('input[name="referralCode"]');
+    var username = $('input[name="username"]');
 
-    $('.validate-form').on('submit',function(){
+    $('.validate-form').on('submit', function (event) {
         var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
+        if (!terms.is(':checked')) {
+            showValidate(terms);
+            check = false;
+        } else {
+            hideValidate(terms);
+        }
+
+        if (!referralCode.val().trim().match(/^[A-Za-z0-9]{6}$/)) {
+            showValidate(referralCode);
+            check = false;
+        } else {
+            hideValidate(referralCode);
+        }
+
+        if (!username.val().trim().match(/^[A-Za-z0-9]{5,}$/)) {
+            showValidate(username);
+            check = false;
+        } else {
+            hideValidate(username);
+        }
+
+        for (var i = 0; i < input.length; i++) {
+            if (input[i] != username[0] && validate(input[i]) == false) {
                 showValidate(input[i]);
-                check=false;
+                check = false;
             }
         }
 
-        return check;
+        if (!check) {
+            event.preventDefault();
+        }
     });
 
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
         });
     });
 
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+    $("#terms").on("change", function() {
+        if (this.checked) {
+            hideValidate(this);
+        }
+    });
+
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
-        }
-        else {
-            if($(input).val().trim() == ''){
+        } else {
+            if ($(input).val().trim() == '') {
                 return false;
             }
         }
@@ -51,7 +79,5 @@
 
         $(thisAlert).removeClass('alert-validate');
     }
-    
-    
 
 })(jQuery);
